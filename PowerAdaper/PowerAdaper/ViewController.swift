d9593d75a42730a82ab7b1ee79e482a19dbca917
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-05-06 20:42:49 +0530

    empty project

---
+++ PowerAdaper/PowerAdaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowerAdaper
+//
+//  Created by sunil.kumar1 on 5/6/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
